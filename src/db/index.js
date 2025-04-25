b4d16ec9b8c82c009595bc5f10e67746a65bfa1f
TyagiManshi <166705490+TyagiManshi@users.noreply.github.com>
2025-04-25 15:25:55 +0530

    corrected the typo

--- src/db/index.js
+++ src/db/index.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URL);
+    await mongoose.connect(process.env.MONGO_URI);
   } catch (error) {
     console.error("Mongodb connetion failed", error);
     process.exit(1);
